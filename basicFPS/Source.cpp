63083495e6a68205feca174a36d1377556da303f
Diope <dionp@fullsail.edu>
2019-08-05 21:02:24 -0400

    Distance calculations for floor and ceiling as well as testing if rays are out of bounds. Fixed a compile error as well

--- basicFPS/Source.cpp
+++ basicFPS/Source.cpp
@@ -9,9 +9,12 @@ int nScreenWidth = 120;
 float playerXpos = 0.0f;
 float playerYpos = 0.0f;
 float playerViewAng = 0.0f;
+float fDepth = 16.0f;
 
 int nMapHeight = 16;
 int nMapWidth = 16;
+
+float fieldFOV = 3.14159f / 4.0f;
 
 int main()
 {
@@ -22,9 +25,68 @@ int main()
 	SetConsoleActiveScreenBuffer(hConsole);
 	DWORD dwBytesWritten = 0;
 
+	wstring map;
+
+	map += L"################";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"################";
+
 	// Write to screen
 	while (1)
 	{
+		for (int i = 0; i < nScreenWidth; i++) 
+		{
+			// Calculate the projected ray angle into world space for each column
+			float fRayAngle = (playerViewAng - fieldFOV / 2.0f) + ((float)i / (float)nScreenWidth) * fieldFOV;
+			float fDistanceToWall = 0;
+            bool bHitWall = false;
+
+			float fEyeX = sinf(fRayAngle);
+			float fEyeY = cosf(fRayAngle);
+
+			while (!bHitWall && fDistanceToWall < fDepth) 
+			{
+               fDistanceToWall += 0.1f;
+
+			   int nTestX = (int)(playerXpos + fEyeX * fDistanceToWall);
+			   int nTestY = (int)(playerYpos + fEyeY * fDistanceToWall);
+
+			   // Test if ray is out of bounds
+			   if (nTestX < 0 || nTestX >= nMapWidth || nTestY < 0 || nTestY >= nMapHeight)
+			   {
+				   bHitWall = true;
+				   fDistanceToWall = fDepth;
+			   }
+			   else 
+			   {
+				   // Ray is inbounds so test to see if the ray cell is a wall block
+				   if (map[nTestY * nMapWidth + nTestX == '#'])
+				   {
+					   bHitWall = true;
+				   }
+			   }
+			}
+
+			// Calc distance to floor and ceiling
+			int nCeiling = (float)(nScreenHeight / 2.0) - nScreenHeight / ((float)fDistanceToWall);
+			int nFloor = nScreenHeight - nCeiling;
+
+
+		}
+
 		screen[nScreenWidth * nScreenHeight - 1] = '\0';
 		WriteConsoleOutputCharacter(hConsole, screen, nScreenHeight * nScreenWidth, { 0, 0 }, &dwBytesWritten);
 	}
